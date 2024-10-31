a247d9b204750ee2f770ea59749baa6167275b94
noOne128 <maksimevstratiev12@outlook.com>
2024-10-31 09:32:14 +0200

    update: add tests for jmp

--- src/test1.cpp
+++ src/test1.cpp
@@ -19,14 +19,30 @@ int main() {
   vm.memory[0x01] = 0x04; // Place for number
   vm.memory[0x02] = 12; // Number
   vm.memory[0x03] = XVM_INSTRUCTION_HALT; // Exit the program safely
-  vm.run(0);
+  vm.run(0x00);
   if (vm.memory[0x04] == 12) {
     std::cout << "\033[92mok";
   } else {
     std::cout << "\033[91merror";
     status = 1;
   }
-  std::cout << "\033[0m\n";
-  if (XVM_CFG_TEST_PRINTDUMP) vm.dump();
+  std::cout << "\033[0m\nChecking jmp instruction...";
+  vm.memory[0x05] = XVM_INSTRUCTION_JMP;
+  vm.memory[0x06] = 0x08;
+  vm.memory[0x07] = XVM_INSTRUCTION_HALT;
+  vm.memory[0x08] = 0;
+  vm.memory[0x09] = XVM_INSTRUCTION_SET;
+  vm.memory[0x0A] = 0x0C;
+  vm.memory[0x0B] = 6;
+  vm.memory[0x0C] = XVM_INSTRUCTION_HALT;
+  vm.run(0x05);
+  if (vm.memory[0x0C] == 6) {
+    std::cout << "\033[92mok";
+  } else {
+    std::cout << "\033[91merror";
+    status = 1;
+  }
+  std::cout << "\033[0m";
+  if (XVM_CFG_TEST_PRINTDUMP && status != 0) vm.dump();
   return status;
 }
