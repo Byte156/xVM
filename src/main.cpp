801e61b7c01d6a219031bd58a05321167483e431
Byte156 <maksimevstratiev12@outlook.com>
2024-11-16 11:12:26 +0000

    feature: run xVM rom files with bin/xvm

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,10 @@
-int main() {
-  // Some interesting stuff planned here, sneak peak: 72736B34
+#include "lib/libxvm.hpp"
+
+int main(int argc, char* argv[]) {
+  if (argc > 1) {
+    xVM vm;
+    vm.read(argv[1], 0);
+    vm.run();
+  }
   return 0;
 }
