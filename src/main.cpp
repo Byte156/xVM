221adbc4fea674a9690166e902742b20d7854094
Byte156 <maksimevstratiev12@outlook.com>
2024-11-18 16:58:45 +0200

    feature: add xvm -d, --dump

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,17 @@
 #include "lib/libxvm.hpp"
+#include <cstring>
 
 int main(int argc, char* argv[]) {
   if (argc > 1) {
     xVM vm;
     vm.read(argv[1], 0);
     vm.run();
+    if (argc > 2) {
+      // Dump if asked
+      if (strcmp(argv[2], "--dump") == 0 || strcmp(argv[2], "-d") == 0) {
+        vm.dump();
+      }
+    }
   }
   return 0;
 }
